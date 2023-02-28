21757f6d9d28cacc52dabc7f19950ad5028ddebb
888-coding <pachelee@gmail.com>
2023-02-28 17:36:38 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #rock papel scissors 
 #application for learn random play with computer.
 
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -31,9 +33,22 @@ scissors = '''
 print("Welcome to Rock Papel Scissors\n")
 mychoice = int(input("Choose your option : 0 - rock , 1 - papel, 2 - scissors\n"))
 
+print("My choice is:")
 if mychoice == 0 :
     print(rock)
 elif mychoice == 1 :
     print(paper)
 else :
     print(scissors)
+
+print("The computer choise is : \n")
+computer_choice = random.randint(0,2)
+if computer_choice == 0 :
+    print(rock)
+elif computer_choice == 1 :
+    print(paper)
+else :
+    print(scissors)
+
+if mychoice == computer_choice :
+    print("You and the computer is draw")
